2bb8a84e856ce7593a3b01f50a05b149b759108b
ImpreYosva <impreyosva@gmail.com>
2026-08-13 09:11:58 -0400

    yaaaa

--- SERVER/config/app.php
+++ SERVER/config/app.php
@@ -99,11 +99,10 @@ return [
 
     'key' => env('APP_KEY'),
 
-    'previous_keys' => [
-        ...array_filter(
-            explode(',', (string) env('APP_PREVIOUS_KEYS', ''))
-        ),
-    ],
+   'previous_keys' => array_filter(
+    explode(',', (string) env('APP_PREVIOUS_KEYS', ''))
+),
+
 
     /*
     |--------------------------------------------------------------------------
